c10c10ffca1b0faa843db0d10af8a31df460ce98
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-14 20:05:35 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,10 +4,4 @@ birthday_kids = {
 "Amanda" => 27
  }
 
-def happy_birthday(birthday_kids)
-birthday_kids.each do |name,birthday|
-  puts "Happy Birthday #{name}! You are now #{age} years old!"
-end 
-end
-
 
